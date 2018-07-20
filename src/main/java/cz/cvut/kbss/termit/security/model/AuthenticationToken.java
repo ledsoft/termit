7b4d37568d79f6334c47d0090b62641ac32e3837
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-07-20 14:36:46 +0200

    Logging configuration, some security model classes and first tests with JUnit 5 setup.

---
+++ src/main/java/cz/cvut/kbss/termit/security/model/AuthenticationToken.java
@@ -0,0 +1,34 @@
+package cz.cvut.kbss.termit.security.model;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.security.Principal;
+import java.util.Collection;
+
+public class AuthenticationToken extends AbstractAuthenticationToken implements Principal {
+
+    private UserDetails userDetails;
+
+    public AuthenticationToken(Collection<? extends GrantedAuthority> authorities, UserDetails userDetails) {
+        super(authorities);
+        this.userDetails = userDetails;
+        super.setAuthenticated(true);
+        super.setDetails(userDetails);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return userDetails.getPassword();
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return userDetails;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/main/java/cz/cvut/kbss/termit/security/model/UserDetails.java
@@ -0,0 +1,66 @@
+package cz.cvut.kbss.termit.security.model;
+
+import cz.cvut.kbss.termit.model.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.*;
+
+public class UserDetails implements org.springframework.security.core.userdetails.UserDetails {
+
+    private User user;
+
+    protected final Set<GrantedAuthority> authorities;
+
+    public UserDetails(User user) {
+        Objects.requireNonNull(user);
+        this.user = user;
+        this.authorities = new HashSet<>();
+    }
+
+    public UserDetails(User user, Collection<GrantedAuthority> authorities) {
+        Objects.requireNonNull(user);
+        Objects.requireNonNull(authorities);
+        this.user = user;
+        this.authorities = new HashSet<>();
+        this.authorities.addAll(authorities);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.unmodifiableCollection(authorities);
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
