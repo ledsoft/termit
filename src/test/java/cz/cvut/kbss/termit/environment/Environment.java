bc28ba1d6c99ab9c4c8766810568735b40c0fcc3
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-07-25 18:15:18 +0200

    Security utilities migrated from RT.

--- src/test/java/cz/cvut/kbss/termit/environment/Environment.java
+++ src/test/java/cz/cvut/kbss/termit/environment/Environment.java
@@ -7,19 +7,56 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.cvut.kbss.jsonld.ConfigParam;
 import cz.cvut.kbss.jsonld.JsonLd;
 import cz.cvut.kbss.jsonld.jackson.JsonLdModule;
+import cz.cvut.kbss.termit.model.User;
+import cz.cvut.kbss.termit.security.model.AuthenticationToken;
+import cz.cvut.kbss.termit.security.model.UserDetails;
 import cz.cvut.kbss.termit.util.Constants;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.ResourceHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
 
 import java.nio.charset.Charset;
+import java.security.Principal;
 import java.util.Collections;
+import java.util.HashSet;
 
 public class Environment {
 
+    private static User currentUser;
+
     private static ObjectMapper objectMapper;
+
+    /**
+     * Initializes security context with the specified user.
+     *
+     * @param user User to set as currently authenticated
+     */
+    public static void setCurrentUser(User user) {
+        currentUser = user;
+        final UserDetails userDetails = new UserDetails(user, new HashSet<>());
+        SecurityContext context = new SecurityContextImpl();
+        context.setAuthentication(new AuthenticationToken(userDetails.getAuthorities(), userDetails));
+        SecurityContextHolder.setContext(context);
+    }
+
+    /**
+     * Gets current user as security principal.
+     *
+     * @return Current user authentication as principal or {@code null} if there is no current user
+     */
+    public static Principal getCurrentUserPrincipal() {
+        return SecurityContextHolder.getContext() != null ? SecurityContextHolder.getContext().getAuthentication() :
+               null;
+    }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
 
     /**
      * Gets a Jackson object mapper for mapping JSON to Java and vice versa.
