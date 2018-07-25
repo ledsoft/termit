e6a5e2f3b3f518161404e482006840ddc81714b9
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-07-25 15:31:43 +0200

    Modified base repository service not to use transactional on read-only methods (prevents issues with postLoad). Implementation of UserRepositoryService.

--- src/test/java/cz/cvut/kbss/termit/service/repository/UserRepositoryServiceTest.java
+++ src/test/java/cz/cvut/kbss/termit/service/repository/UserRepositoryServiceTest.java
@@ -2,6 +2,7 @@ package cz.cvut.kbss.termit.service.repository;
 
 import cz.cvut.kbss.jopa.model.EntityManager;
 import cz.cvut.kbss.termit.environment.Generator;
+import cz.cvut.kbss.termit.exception.NotFoundException;
 import cz.cvut.kbss.termit.exception.ValidationException;
 import cz.cvut.kbss.termit.model.User;
 import cz.cvut.kbss.termit.service.BaseServiceTestRunner;
@@ -13,9 +14,7 @@ import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserRepositoryServiceTest extends BaseServiceTestRunner {
 
@@ -29,26 +28,23 @@ class UserRepositoryServiceTest extends BaseServiceTestRunner {
     private PasswordEncoder passwordEncoder;
 
     @Test
-    void findByUsernameReturnsUserWithMatchingUsername() {
+    void existsByUsernameReturnsTrueForExistingUsername() {
         final User user = Generator.generateUser();
         user.setUri(Generator.generateUri());
         transactional(() -> em.persist(user));
 
-        final Optional<User> result = sut.findByUsername(user.getUsername());
-        assertTrue(result.isPresent());
-        assertEquals(user, result.get());
+        assertTrue(sut.exists(user.getUsername()));
     }
 
     @Test
     void persistEncodesUserPassword() {
         final User user = Generator.generateUser();
         user.setUri(Generator.generateUri());
-        final String oldPassword = user.getPassword();
+        final String plainPassword = user.getPassword();
 
         sut.persist(user);
-        final Optional<User> result = sut.find(user.getUri());
-        assertTrue(result.isPresent());
-        assertTrue(passwordEncoder.matches(oldPassword, result.get().getPassword()));
+        final User result = em.find(User.class, user.getUri());
+        assertTrue(passwordEncoder.matches(plainPassword, result.getPassword()));
     }
 
     @Test
@@ -67,5 +63,69 @@ class UserRepositoryServiceTest extends BaseServiceTestRunner {
         user.setPassword("");
         final ValidationException ex = assertThrows(ValidationException.class, () -> sut.persist(user));
         assertThat(ex.getMessage(), containsString("password must not be empty"));
+    }
+
+    @Test
+    void updateEncodesPasswordWhenItWasChanged() {
+        final User user = Generator.generateUser();
+        user.setUri(Generator.generateUri());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        transactional(() -> em.persist(user));
+        final String plainPassword = "updatedPassword01";
+        user.setPassword(plainPassword);
+
+        sut.update(user);
+        final User result = em.find(User.class, user.getUri());
+        assertTrue(passwordEncoder.matches(plainPassword, result.getPassword()));
+    }
+
+    @Test
+    void updateRetainsOriginalPasswordWhenItDoesNotChange() {
+        final User user = Generator.generateUser();
+        user.setUri(Generator.generateUri());
+        final String plainPassword = user.getPassword();
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        transactional(() -> em.persist(user));
+        user.setPassword(null); // Simulate instance being loaded from repo
+        final String newLastName = "newLastName";
+        user.setLastName(newLastName);
+
+        sut.update(user);
+        final User result = em.find(User.class, user.getUri());
+        assertTrue(passwordEncoder.matches(plainPassword, result.getPassword()));
+        assertEquals(newLastName, result.getLastName());
+    }
+
+    @Test
+    void updateThrowsNotFoundExceptionWhenUserDoesNotExist() {
+        final User user = Generator.generateUser();
+        user.setUri(Generator.generateUri());
+        final NotFoundException ex = assertThrows(NotFoundException.class, () -> sut.update(user));
+        assertEquals("User " + user + " does not exist.", ex.getMessage());
+    }
+
+    @Test
+    void postLoadErasesPasswordFromInstance() {
+        final User user = Generator.generateUser();
+        user.setUri(Generator.generateUri());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        transactional(() -> em.persist(user));
+
+        final Optional<User> result = sut.find(user.getUri());
+        assertTrue(result.isPresent());
+        assertNull(result.get().getPassword());
+    }
+
+    @Test
+    void updateThrowsValidationExceptionWhenUpdatedInstanceIsMissingValues() {
+        final User user = Generator.generateUser();
+        user.setUri(Generator.generateUri());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        transactional(() -> em.persist(user));
+
+        user.setUsername(null);
+        user.setPassword(null); // Simulate instance being loaded from repo
+        final ValidationException ex = assertThrows(ValidationException.class, () -> sut.update(user));
+        assertThat(ex.getMessage(), containsString("username must not be empty"));
     }
 }
