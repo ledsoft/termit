31ecb126bd4f6b9ae7da355a8a7e55a6980c5bcd
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-07-27 12:51:33 +0200

    Added security configuration. Added missing war packaging into pom.xml.

--- src/main/java/cz/cvut/kbss/termit/config/SecurityConfig.java
+++ src/main/java/cz/cvut/kbss/termit/config/SecurityConfig.java
@@ -1,7 +1,81 @@
 package cz.cvut.kbss.termit.config;
 
+import cz.cvut.kbss.termit.security.*;
+import cz.cvut.kbss.termit.service.security.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-public class SecurityConfig {
+@ComponentScan(basePackageClasses = Security.class)
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AuthenticationProvider authenticationProvider;
+
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+
+    private final AuthenticationSuccess authenticationSuccessHandler;
+
+    private final AuthenticationFailureHandler authenticationFailureHandler;
+
+    private final JwtUtils jwtUtils;
+
+    private final SecurityUtils securityUtils;
+
+    @Autowired
+    public SecurityConfig(AuthenticationProvider authenticationProvider,
+                          AuthenticationEntryPoint authenticationEntryPoint,
+                          AuthenticationSuccess authenticationSuccessHandler,
+                          AuthenticationFailureHandler authenticationFailureHandler,
+                          JwtUtils jwtUtils, SecurityUtils securityUtils) {
+        this.authenticationProvider = authenticationProvider;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+        this.authenticationFailureHandler = authenticationFailureHandler;
+        this.jwtUtils = jwtUtils;
+        this.securityUtils = securityUtils;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll().and()
+            .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+            .and().cors().and().csrf().disable()
+            .addFilter(new JwtAuthenticationFilter(jwtUtils))
+            .addFilter(new JwtAuthorizationFilter(authenticationManager(), jwtUtils, securityUtils))
+            .formLogin().successHandler(authenticationSuccessHandler)
+            .failureHandler(authenticationFailureHandler)
+            .loginProcessingUrl(SecurityConstants.SECURITY_CHECK_URI)
+            .usernameParameter(SecurityConstants.USERNAME_PARAM).passwordParameter(SecurityConstants.PASSWORD_PARAM)
+            .and().logout().logoutUrl(SecurityConstants.LOGOUT_URI).logoutSuccessHandler(authenticationSuccessHandler)
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
 }
