52400f0f301c22877c8e3a7f3c4cbac1519eafff
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-09-14 17:09:05 +0200

    [Upd] Store each vocabulary in its own context.

--- src/main/java/cz/cvut/kbss/termit/persistence/dao/VocabularyDao.java
+++ src/main/java/cz/cvut/kbss/termit/persistence/dao/VocabularyDao.java
@@ -1,12 +1,16 @@
 package cz.cvut.kbss.termit.persistence.dao;
 
 import cz.cvut.kbss.jopa.model.EntityManager;
+import cz.cvut.kbss.jopa.model.descriptors.Descriptor;
+import cz.cvut.kbss.jopa.model.descriptors.EntityDescriptor;
+import cz.cvut.kbss.termit.exception.PersistenceException;
 import cz.cvut.kbss.termit.model.Vocabulary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class VocabularyDao extends BaseDao<Vocabulary> {
@@ -21,5 +25,32 @@ public class VocabularyDao extends BaseDao<Vocabulary> {
         final List<Vocabulary> result = super.findAll();
         result.sort(Comparator.comparing(Vocabulary::getName));
         return result;
+    }
+
+    @Override
+    public void persist(Vocabulary entity) {
+        Objects.requireNonNull(entity);
+        try {
+            em.persist(entity, descriptorFor(entity));
+        } catch (RuntimeException e) {
+            throw new PersistenceException(e);
+        }
+    }
+
+    private Descriptor descriptorFor(Vocabulary entity) {
+        final EntityDescriptor descriptor = new EntityDescriptor(entity.getUri());
+        descriptor.addAttributeContext(
+                em.getMetamodel().entity(Vocabulary.class).getAttribute("author").getJavaField(), null);
+        return descriptor;
+    }
+
+    @Override
+    public Vocabulary update(Vocabulary entity) {
+        Objects.requireNonNull(entity);
+        try {
+            return em.merge(entity, descriptorFor(entity));
+        } catch (RuntimeException e) {
+            throw new PersistenceException(e);
+        }
     }
 }
