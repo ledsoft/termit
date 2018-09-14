1f781280627579018b2750eff5f946b7f979f3d4
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-09-14 19:28:20 +0200

    [Task #684] Basic text analysis service invocation with tests.

--- src/main/java/cz/cvut/kbss/termit/service/TextAnalysisService.java
+++ src/main/java/cz/cvut/kbss/termit/service/TextAnalysisService.java
@@ -1,11 +1,21 @@
 package cz.cvut.kbss.termit.service;
 
+import cz.cvut.kbss.termit.dto.TextAnalysisInput;
 import cz.cvut.kbss.termit.model.File;
 import cz.cvut.kbss.termit.model.Vocabulary;
+import cz.cvut.kbss.termit.util.ConfigParam;
 import cz.cvut.kbss.termit.util.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class TextAnalysisService {
@@ -30,6 +40,22 @@ public class TextAnalysisService {
      * @param vocabulary Vocabulary used for text analysis
      */
     public void analyzeDocument(File file, Vocabulary vocabulary) {
+        Objects.requireNonNull(file);
+        Objects.requireNonNull(vocabulary);
+        final TextAnalysisInput input = new TextAnalysisInput();
+        input.setContent(loadFileContent(file));
+        input.setVocabularyContext(vocabulary.getUri());
+        input.setVocabularyRepository(URI.create(config.get(ConfigParam.REPOSITORY_URL)));
+        restClient.exchange(config.get(ConfigParam.TEXT_ANALYSIS_SERVICE_URL), HttpMethod.POST, new HttpEntity<>(input),
+                Object.class);
+    }
 
+    private String loadFileContent(File file) {
+        try {
+            final List<String> lines = Files.readAllLines(new java.io.File(file.getLocation()).toPath());
+            return String.join("\n", lines);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
