ed021edc3b6249eb9ad9ccc59ea1fdc2b77e7df5
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-09-21 17:04:30 +0200

    [Task #687] Store the content processed by annotation processor, so that any updates to it (e.g. new term occurrences) are present in the saved artifact. Did some reverts in TermService to prevent merging/lazy loading issues. Temporarily disabled user credentials erasure due to possible bug in JOPA.

--- src/main/java/cz/cvut/kbss/termit/service/document/AnnotationGenerator.java
+++ src/main/java/cz/cvut/kbss/termit/service/document/AnnotationGenerator.java
@@ -1,11 +1,10 @@
 package cz.cvut.kbss.termit.service.document;
 
 import cz.cvut.kbss.termit.exception.AnnotationGenerationException;
-import cz.cvut.kbss.termit.model.File;
-import cz.cvut.kbss.termit.model.Term;
-import cz.cvut.kbss.termit.model.TermOccurrence;
-import cz.cvut.kbss.termit.model.Vocabulary;
+import cz.cvut.kbss.termit.exception.TermItException;
+import cz.cvut.kbss.termit.model.*;
 import cz.cvut.kbss.termit.persistence.dao.TermOccurrenceDao;
+import cz.cvut.kbss.termit.service.repository.DocumentRepositoryService;
 import cz.cvut.kbss.termit.service.repository.TermRepositoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +13,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 /**
@@ -32,42 +34,57 @@ public class AnnotationGenerator {
 
     private final TermOccurrenceDao termOccurrenceDao;
 
+    private final DocumentRepositoryService documentService;
+
     private final TermOccurrenceResolver htmlOccurrenceResolver;
 
     @Autowired
     public AnnotationGenerator(TermRepositoryService termService,
                                TermOccurrenceDao termOccurrenceDao,
+                               DocumentRepositoryService documentService,
                                @Qualifier("html") TermOccurrenceResolver htmlOccurrenceResolver) {
         this.termService = termService;
         this.termOccurrenceDao = termOccurrenceDao;
+        this.documentService = documentService;
         this.htmlOccurrenceResolver = htmlOccurrenceResolver;
     }
 
     /**
      * Generates annotations (term occurrences) for terms identified in the specified document.
      *
-     * @param content    Content of file with identified term occurrences
-     * @param source     Source file of the annotated document
-     * @param vocabulary Vocabulary whose terms occur in the document
+     * @param content  Content of file with identified term occurrences
+     * @param source   Source file of the annotated document
+     * @param document Logical document which contains content
      */
     @Transactional
-    public void generateAnnotations(InputStream content, File source, Vocabulary vocabulary) {
+    public void generateAnnotations(InputStream content, File source, Document document) {
         // This will allow us to potentially support different types of files
         if (htmlOccurrenceResolver.supports(source)) {
             LOG.debug("Resolving annotations of HTML file {}.", source);
             htmlOccurrenceResolver.parseContent(content, source);
-            final List<Term> newTerms = htmlOccurrenceResolver.findNewTerms(vocabulary);
+            final List<Term> newTerms = htmlOccurrenceResolver.findNewTerms(document.getVocabulary());
             newTerms.forEach(t -> {
                 t.addType(cz.cvut.kbss.termit.util.Vocabulary.s_c_navrzeny_term);
-                termService.addTermToVocabulary(t, vocabulary);
+                termService.addTermToVocabulary(t, document.getVocabulary().getUri());
             });
             final List<TermOccurrence> occurrences = htmlOccurrenceResolver.findTermOccurrences();
             occurrences.forEach(o -> {
                 o.addType(cz.cvut.kbss.termit.util.Vocabulary.s_c_navrzeny_vyskyt_termu);
                 termOccurrenceDao.persist(o);
             });
+            saveAnnotatedContent(htmlOccurrenceResolver.getContent(), source, document);
         } else {
             throw new AnnotationGenerationException("Unsupported type of file " + source);
+        }
+    }
+
+    private void saveAnnotatedContent(InputStream input, File file, Document document) {
+        try {
+            final java.io.File content = documentService.resolveFile(document, file);
+            LOG.debug("Saving annotated content to {}.", content);
+            Files.copy(input, content.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new TermItException("Unable to write out text analysis results.", e);
         }
     }
 }
