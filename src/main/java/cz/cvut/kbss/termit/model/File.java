54ed8b1b0ab8a5e0dfedfdfbaddae90843ae71fc
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-10-09 13:47:24 +0200

    [Upd] Added inverse property pointing from file to document, so that document is accessible from file.

--- src/main/java/cz/cvut/kbss/termit/model/File.java
+++ src/main/java/cz/cvut/kbss/termit/model/File.java
@@ -1,9 +1,7 @@
 package cz.cvut.kbss.termit.model;
 
-import cz.cvut.kbss.jopa.model.annotations.OWLClass;
-import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
-import cz.cvut.kbss.jopa.model.annotations.OWLObjectProperty;
-import cz.cvut.kbss.jopa.model.annotations.ParticipationConstraints;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import cz.cvut.kbss.jopa.model.annotations.*;
 import cz.cvut.kbss.jopa.vocabulary.RDFS;
 import cz.cvut.kbss.termit.util.Vocabulary;
 
@@ -21,8 +19,6 @@ public class File extends AbstractEntity {
     @OWLDataProperty(iri = Vocabulary.s_p_ma_nazev_souboru)
     private String fileName;
 
-    // TODO Add inverse property pointing to the parent document. It will simplify the DocumentManager API
-
     /**
      * File origin.
      * <p>
@@ -30,6 +26,11 @@ public class File extends AbstractEntity {
      */
     @OWLObjectProperty(iri = Vocabulary.s_p_ma_puvod)
     private URI origin;
+
+    @JsonIgnore
+    @Inferred
+    @OWLObjectProperty(iri = Vocabulary.s_p_je_casti_dokumentu)
+    private Document document;
 
     public String getComment() {
         return comment;
@@ -53,6 +54,14 @@ public class File extends AbstractEntity {
 
     public void setOrigin(URI origin) {
         this.origin = origin;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
     }
 
     @Override
