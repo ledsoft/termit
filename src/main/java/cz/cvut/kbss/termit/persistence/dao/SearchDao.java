8e15691b2c994f18bbe1f953bf4409297695a72e
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-12-01 17:30:47 +0100

    [Feature #649] Externalized fulltext search query into a file and added Maven profiles for default, RDF4J with Lucene and GraphDB with Lucene search.

--- src/main/java/cz/cvut/kbss/termit/persistence/dao/SearchDao.java
+++ src/main/java/cz/cvut/kbss/termit/persistence/dao/SearchDao.java
@@ -2,20 +2,46 @@ package cz.cvut.kbss.termit.persistence.dao;
 
 import cz.cvut.kbss.jopa.model.EntityManager;
 import cz.cvut.kbss.termit.dto.FullTextSearchResult;
+import cz.cvut.kbss.termit.exception.TermItException;
+import cz.cvut.kbss.termit.util.Constants;
+import cz.cvut.kbss.termit.util.Vocabulary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
+import java.io.*;
+import java.net.URI;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Repository
 public class SearchDao {
+
+    private static final String FTS_QUERY_FILE = "fulltextsearch.rq";
+
+    private String ftsQuery;
 
     private final EntityManager em;
 
     @Autowired
     public SearchDao(EntityManager em) {
         this.em = em;
+    }
+
+    @PostConstruct
+    private void loadQueries() {
+        final InputStream is = getClass().getClassLoader().getResourceAsStream(
+                Constants.QUERY_DIRECTORY + File.separator + FTS_QUERY_FILE);
+        if (is == null) {
+            throw new TermItException(
+                    "Initialization exception. Full text search query not found in " + Constants.QUERY_DIRECTORY + File.separator + FTS_QUERY_FILE);
+        }
+        try (final BufferedReader in = new BufferedReader(new InputStreamReader(is))) {
+            this.ftsQuery = in.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new TermItException("Initialization exception. Unable to load full text search query!", e);
+        }
     }
 
     /**
@@ -28,28 +54,11 @@ public class SearchDao {
      */
     public List<FullTextSearchResult> fullTextSearch(String searchString) {
         Objects.requireNonNull(searchString);
-        return (List<FullTextSearchResult>) em.createNativeQuery("PREFIX : <http://www.ontotext.com/connectors/lucene#>\n" +
-                "PREFIX inst: <http://www.ontotext.com/connectors/lucene/instance#>\n" +
-                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-                "PREFIX pdp:<http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/>\n" +
-                "\n" +
-                "SELECT DISTINCT ?entity ?label ?snippetText ?snippetField ?score ?type ?vocabularyURI {\n" +
-                "  ?search1 a inst:czech_index ;\n" +
-                "      :query ?searchString ;\n" +
-                "      :entities ?entity .\n" +
-                "  ?entity a ?type .\n" +
-                "  ?entity rdfs:label ?label . \n" +
-                "  ?entity :score ?score .\n" +
-                "  ?entity :snippets _:s .\n" +
-                "        _:s :snippetText ?snippetText .\n" +
-                "    _:s :snippetField ?snippetField .\n" +
-                "    OPTIONAL {\n" +
-                "        ?entity pdp:je-pojmem-ze-slovniku ?vocabularyURI .\n" +
-                "    }\n" +
-                "    FILTER (?type = pdp:term || ?type = pdp:slovnik)\n" +
-                "          \n" +
-                "}\n" +
-                "ORDER BY desc(?score)", "FullTextSearchResult")
-                                           .setParameter("searchString", searchString, null).getResultList();
+        return (List<FullTextSearchResult>) em.createNativeQuery(ftsQuery, "FullTextSearchResult")
+                                              .setParameter("term", URI.create(Vocabulary.s_c_term))
+                                              .setParameter("vocabulary", URI.create(Vocabulary.s_c_slovnik))
+                                              .setParameter("inVocabulary",
+                                                      URI.create(Vocabulary.s_p_je_pojmem_ze_slovniku))
+                                              .setParameter("searchString", searchString, null).getResultList();
     }
 }
