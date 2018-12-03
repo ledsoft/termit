41af5af3680b592b06ac8637099c4cfec77416e2
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-12-03 17:00:25 +0100

    [Upd] SCA-based code improvements.

--- src/main/java/cz/cvut/kbss/termit/security/AuthenticationSuccess.java
+++ src/main/java/cz/cvut/kbss/termit/security/AuthenticationSuccess.java
@@ -2,7 +2,6 @@ package cz.cvut.kbss.termit.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.cvut.kbss.termit.security.model.LoginStatus;
-import cz.cvut.kbss.termit.security.model.TermItUserDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
