2309248d3da6c05a0158a0affa6ea90220673657
Petr Kremen <kremep1@fel.cvut.cz>
2019-01-24 18:32:59 +0100

    [UPD] finishing transformation from modelio.

--- src/main/resources/transform-sgov/transform.sh
+++ src/main/resources/transform-sgov/transform.sh
@@ -2,6 +2,13 @@
 RDF4J_ENDPOINT=http://onto.fel.cvut.cz:7200/repositories/termit-stage
 DIR="/home/kremep1/fel/projects/17mvcr/semanticky-slovnik-pojmu/model/IPR Praha/"
 IFS=
+FILE=tmp.rq
+
+function runUpdateQuery() {
+    cp $1 $FILE
+    sed -i -e "s!__VOC_IRI__!$2!g" $FILE
+    curl -X POST -H "Content-type: application/sparql-update" -T $FILE $RDF4J_ENDPOINT/statements
+}
 
 function transform() {
     VOC_IRI=$1
@@ -12,22 +19,18 @@ function transform() {
     curl -X POST -H "Content-type: text/turtle" -T "$DIR/$PREFIX-glosář.ttl" $URL
     curl -X POST -H "Content-type: text/turtle" -T "$DIR/$PREFIX-slovník.ttl" $URL
 
-    FILE=tmp.rq
-    cp transform-1.rq $FILE
-    sed -i -e "s!__VOC_IRI__!$VOC_IRI!g" $FILE
-    curl -X POST -H "Content-type: application/sparql-update" -T $FILE $RDF4J_ENDPOINT/statements
-
-    cp transform-2.rq $FILE
-    sed -i -e "s!__VOC_IRI__!$VOC_IRI!g" $FILE
-    curl -X POST -H "Content-type: application/sparql-update" -T $FILE $RDF4J_ENDPOINT/statements
-
-    cp transform-3.rq $FILE
-    sed -i -e "s!__VOC_IRI__!$VOC_IRI!g" $FILE
-    curl -X POST -H "Content-type: application/sparql-update" -T $FILE $RDF4J_ENDPOINT/statements
+    runUpdateQuery transform-2.rq $VOC_IRI
+    runUpdateQuery transform-4.rq $VOC_IRI
+    runUpdateQuery transform-1.rq $VOC_IRI
+    runUpdateQuery transform-3.rq $VOC_IRI
 }
 
-# transform http://onto.fel.cvut.cz/ontologies/slovnik/legislativni-sbirka-2006-183 l-sgov-183-2006
-# transform http://onto.fel.cvut.cz/ontologies/slovnik/legislativni-sbirka-2006-500 l-sgov-500-2006
-# transform http://onto.fel.cvut.cz/ontologies/slovnik/legislativni-sbirka-2006-501 l-sgov-501-2006
-# transform http://onto.fel.cvut.cz/ontologies/slovnik/datovy-psp-2016 psp-2016
-# transform http://onto.fel.cvut.cz/ontologies/slovnik/datovy-mpp-3.4 mpp-3.4
+URL="$RDF4J_ENDPOINT/rdf-graphs/service?graph=http://onto.fel.cvut.cz/ontologies/termit"
+TERMIT=$(pwd)
+curl -X POST -H "Content-type: text/turtle" -T "$TERMIT/../../../../termit.ttl" $URL
+
+transform http://onto.fel.cvut.cz/ontologies/slovnik/legislativni-sbirka-2006-183 l-sgov-183-2006
+transform http://onto.fel.cvut.cz/ontologies/slovnik/legislativni-sbirka-2006-500 l-sgov-500-2006
+transform http://onto.fel.cvut.cz/ontologies/slovnik/legislativni-sbirka-2006-501 l-sgov-501-2006
+transform http://onto.fel.cvut.cz/ontologies/slovnik/datovy-psp-2016 psp-2016
+transform http://onto.fel.cvut.cz/ontologies/slovnik/datovy-mpp-3.4 mpp-3.4
