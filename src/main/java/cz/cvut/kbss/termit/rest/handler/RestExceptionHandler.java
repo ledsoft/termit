aa7be22a915350eff041df2de4cd918043d7d33b
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2019-01-28 18:49:32 +0100

    Removed unused import.

--- src/main/java/cz/cvut/kbss/termit/rest/handler/RestExceptionHandler.java
+++ src/main/java/cz/cvut/kbss/termit/rest/handler/RestExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
 
