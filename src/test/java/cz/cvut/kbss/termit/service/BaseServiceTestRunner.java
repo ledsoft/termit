5525d5d2d20da617bb9e711486544a5baa6b5217
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2019-02-28 16:48:19 +0100

    [Upd] Allow to use RDFS inference in tests (see README for details).

--- src/test/java/cz/cvut/kbss/termit/service/BaseServiceTestRunner.java
+++ src/test/java/cz/cvut/kbss/termit/service/BaseServiceTestRunner.java
@@ -1,29 +1,20 @@
 package cz.cvut.kbss.termit.service;
 
-import cz.cvut.kbss.termit.environment.Transaction;
+import cz.cvut.kbss.termit.environment.TransactionalTestRunner;
 import cz.cvut.kbss.termit.environment.config.TestConfig;
 import cz.cvut.kbss.termit.environment.config.TestPersistenceConfig;
 import cz.cvut.kbss.termit.environment.config.TestServiceConfig;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@ExtendWith(SpringExtension.class)
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableTransactionManagement
-@ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {TestConfig.class, TestPersistenceConfig.class, TestServiceConfig.class})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public class BaseServiceTestRunner {
-
-    @Autowired
-    protected PlatformTransactionManager txManager;
-
-    protected void transactional(Runnable procedure) {
-        Transaction.execute(txManager, procedure);
-    }
+public class BaseServiceTestRunner extends TransactionalTestRunner {
 }
