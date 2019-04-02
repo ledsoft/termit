f73a463bac9461e0117925149d572d8325b1467d
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2019-04-02 08:53:46 +0200

    [Upd] Do not require authentication when getting possible types.

--- src/main/java/cz/cvut/kbss/termit/rest/LanguageController.java
+++ src/main/java/cz/cvut/kbss/termit/rest/LanguageController.java
@@ -7,6 +7,7 @@ import cz.cvut.kbss.termit.service.language.LanguageService;
 import cz.cvut.kbss.termit.util.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,6 +33,7 @@ public class LanguageController extends BaseController {
     /**
      * @return List of types
      */
+    @PreAuthorize("permitAll()")    // No need to secure this
     @RequestMapping(value = "/types", method = RequestMethod.GET,
             produces = {MediaType.APPLICATION_JSON_VALUE, JsonLd.MEDIA_TYPE})
     public List<Term> getAll(@RequestParam String language) {
