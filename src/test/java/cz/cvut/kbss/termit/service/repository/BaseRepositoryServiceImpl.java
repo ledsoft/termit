9447cd777bc0dcdf2dfffedc32e8129691497579
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2019-04-08 09:14:31 +0200

    [Fix] Minor compilation issues fix.

--- src/test/java/cz/cvut/kbss/termit/service/repository/BaseRepositoryServiceImpl.java
+++ src/test/java/cz/cvut/kbss/termit/service/repository/BaseRepositoryServiceImpl.java
@@ -18,13 +18,14 @@
 package cz.cvut.kbss.termit.service.repository;
 
 import cz.cvut.kbss.termit.model.User;
+import cz.cvut.kbss.termit.model.UserAccount;
 import cz.cvut.kbss.termit.persistence.dao.GenericDao;
 import cz.cvut.kbss.termit.persistence.dao.UserAccountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.Validator;
 
-public class BaseRepositoryServiceImpl extends BaseRepositoryService<User> {
+public class BaseRepositoryServiceImpl extends BaseRepositoryService<UserAccount> {
 
     private final UserAccountDao userAccountDao;
 
@@ -35,7 +36,7 @@ public class BaseRepositoryServiceImpl extends BaseRepositoryService<User> {
     }
 
     @Override
-    protected GenericDao<User> getPrimaryDao() {
+    protected GenericDao<UserAccount> getPrimaryDao() {
         return userAccountDao;
     }
 }
