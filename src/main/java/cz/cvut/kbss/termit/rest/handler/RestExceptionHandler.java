ca7a1e0b7caae90640e64366adff1352b635a58e
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-05-20 08:16:51 +0200

    [Fix] Fix merge issues.

--- src/main/java/cz/cvut/kbss/termit/rest/handler/RestExceptionHandler.java
+++ src/main/java/cz/cvut/kbss/termit/rest/handler/RestExceptionHandler.java
@@ -16,7 +16,9 @@ package cz.cvut.kbss.termit.rest.handler;
 
 import cz.cvut.kbss.jopa.exceptions.OWLPersistenceException;
 import cz.cvut.kbss.jsonld.exception.JsonLdException;
+import cz.cvut.kbss.termit.exception.UnsupportedOperationException;
 import cz.cvut.kbss.termit.exception.*;
+import cz.cvut.kbss.termit.exception.workspace.WorkspaceNotSetException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
